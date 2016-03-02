8c6599ed874bd6f07ef49ecbb2e7b9dfeb8b12df
Lucasljj <lucasljj@gmail.com>
2016-03-02 14:12:59 -0300

    unsed dependencies removed

--- src/RoleExpireStorage.php
+++ src/RoleExpireStorage.php
@@ -3,12 +3,6 @@ declare(strict_types = 1);
 
 namespace Drupal\role_expire;
 
-use Drupal\Core\Cache\CacheBackendInterface;
-use Drupal\Core\Database\Connection;
-use Drupal\Core\Entity\EntityManagerInterface;
-use Drupal\Core\Entity\EntityTypeInterface;
 use Drupal\Core\Entity\Sql\SqlContentEntityStorage;
-use Drupal\Core\Language\LanguageManagerInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class RoleExpireStorage extends SqlContentEntityStorage {}
